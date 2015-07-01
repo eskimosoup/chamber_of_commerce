b2c367b7cad512a1ce40dd876db73f0f7f995cb2
James Doyley <jdoyley@gmail.com>
2015-07-01 12:50:42 +0100

    Update article tests

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -7,7 +7,10 @@ class ArticleTest < ActiveSupport::TestCase
   should validate_presence_of(:date)
   should belong_to(:category)
 
-  #should validate_uniqueness_of(:suggested_url).allow_blank.with_message('is not unique, leave this blank to generate automatically')
+  should "validate uniqueness" do
+    Article.new(title: "title")
+    validate_uniqueness_of(:suggested_url).allow_blank.with_message('is not unique, leave this blank to generate automatically')
+  end
 
 
 end
