d05279842272f2ebb4b73bb3d770bf247d242c93
Optimised server <support@eskimosoup.co.uk>
2015-07-30 13:51:57 +0100

    Page style list

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,8 @@ class PagesController < ApplicationController
   def show
     @page_object = Page.where(display: true).find(params[:id])
     @page = PagePresenter.new(object: @page_object, view_template: view_context)
-    if request.path != page_path(@page_object)
-      return redirect_to @page_object, status: :moved_permanently
-    else
-      render @page_object.style
-    end
+    @members_services_menu = Optimadmin::Menu.new(name: "members_services") if @page_object.style == 'members_services'
+    return redirect_to @page_object, status: :moved_permanently if request.path != page_path(@page_object)
+    render @page_object.style
   end
 end
