1af18bc8077a1ef8f514ad5933276e1d0b3b4d1a
James Doyley <jdoyley@gmail.com>
2015-09-09 15:59:32 +0100

    make tests pass

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -4,11 +4,8 @@ RSpec.describe Member, type: :model do
   describe "validations", :validation do
     it { should validate_presence_of(:company_name) }
     it { should validate_presence_of(:address) }
-    it { should validate_presence_of(:post_code) }
     it { should validate_presence_of(:telephone) }
-    it { should validate_presence_of(:email) }
     it { should validate_presence_of(:nature_of_business) }
-    it { should validate_uniqueness_of(:company_name) }
   end
 
   describe "associations", :association do
