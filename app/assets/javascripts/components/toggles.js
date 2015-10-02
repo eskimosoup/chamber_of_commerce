3e259f7d10cb353b7dcf4751bf645fa26e3e224a
Optimised server <support@eskimosoup.co.uk>
2015-10-02 11:10:29 +0100

    Billing address, step-based booking process and make as Free instead of N/A

--- app/assets/javascripts/components/toggles.js
+++ app/assets/javascripts/components/toggles.js
@@ -17,3 +17,30 @@ $(document).on("click", ".toggle-class", function(e) {
     e.preventDefault();
   }
 });
+
+$(document).on("click", ".add-event-attendees", function(e) {
+  /*
+   * Example usage:
+   * <a href="#div-to-go-to" data-container="#id" data-class="hidden" data-return="false">Toggle .hidden on #id and stop</a>
+   *
+   * Alternatively:
+   * <a href="#div-to-go-to" data-container=".links" data-class="hidden" data-return="true">Toggle .hidden on .links and scroll to #div-to-go-to</a>
+   */
+
+  var toggleContainer = $(this).attr('data-container');
+  var toggleClass     = $(this).attr('data-class');
+  var dataReturn      = $(this).attr('data-return');
+
+  $(toggleContainer).toggleClass(toggleClass);
+
+  if($(this).attr('data-clicked') === undefined) {
+    $('.add_fields').click();
+    setMainBookerDetails();
+    $('.booker-population, .remove_fields').hide();
+    $(this).attr('data-clicked', 'true');
+  }
+
+  if (dataReturn === 'false') {
+    e.preventDefault();
+  }
+});
