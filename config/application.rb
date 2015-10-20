83e18204f18711fe6e1b3a5f25171ed3a21fb866
Optimised server <support@eskimosoup.co.uk>
2015-10-20 15:11:19 +0100

    Update time zone

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,6 @@ module ChamberOfCommerce
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.exceptions_app = self.routes
+    Time.zone = 'London'
   end
 end
