34de1b88db3a7030840d942e4b1e739221c6c295
Optimised server <support@eskimosoup.co.uk>
2015-11-10 16:01:14 +0000

    Add contact name

--- app/models/member_login.rb
+++ app/models/member_login.rb
@@ -6,6 +6,7 @@ class MemberLogin < ActiveRecord::Base
   validates :member, presence: { message: "company can't be blank" },
                      uniqueness: { message: 'company already has a login' }
   validates :username, presence: true, uniqueness: true
+  validates :contact_name, presence: true
 
   def generate_auth_token
     self.auth_token = secure_auth_token
