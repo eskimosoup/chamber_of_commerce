d7d34d6f8f801887d7473787053aca84efc555b8
optimised <noreply@optimised.today>
2017-03-08 09:53:12 +0000

    Add booking information confirmation, update mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,19 +1,16 @@
 Rails.application.configure do
   Rails.application.routes.default_url_options[:host] = '192.168.0.26:5034'
-  config.action_mailer.smtp_settings = {:enable_starttls_auto => false}
-  config.action_mailer.default_url_options = { host: "localhost:3000" }
-  config.action_mailer.asset_host = "http://localhost:3000"
-
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {:address => 'mail.eskimosoup.co.uk', :authentication => :plain, :user_name => 'tasks@eskimosoup.co.uk', :password => 'poipoip'}
+  config.action_mailer.smtp_settings = { enable_starttls_auto: false }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.asset_host = 'http://localhost:3000'
 
   config.generators do |g|
     g.assets false
-    g.stylesheets  false
+    g.stylesheets false
     g.helper false
   end
 
-  config.logger = Logger.new(config.paths['log'].first, 3, 5242880)
+  config.logger = Logger.new(config.paths['log'].first, 3, 5_242_880)
 
   # Settings specified here will take precedence over those in config/application.rb.
 
