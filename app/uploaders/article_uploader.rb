38aa46aede145dbd3bf0e64304569d3f8e7c4094
Paul <pauloptimised@users.noreply.github.com>
2018-12-13 10:39:03 +0000

    Update image crops

--- app/uploaders/article_uploader.rb
+++ app/uploaders/article_uploader.rb
@@ -8,14 +8,13 @@ class ArticleUploader < Optimadmin::ImageUploader
   version :show do
     process resize_to_fill: [215, 135]
   end
+=end
 
   version :show_full_image do
     process resize_to_fit: [613, 9999]
   end
-=end
 
   version :homepage do
     process resize_to_fill: [450, 295]
   end
-
 end
