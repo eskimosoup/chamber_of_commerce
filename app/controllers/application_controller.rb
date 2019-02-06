6b8c393ea1c866f6e9bafe9a20a5cc10639d83a9
Paul <pauloptimised@users.noreply.github.com>
2019-02-06 17:01:08 +0000

    Amend ads

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -63,7 +63,7 @@ class ApplicationController < ActionController::Base
     @header_menu = Optimadmin::Menu.new(name: 'header')
     @footer_menu = Optimadmin::Menu.new(name: 'footer')
     @newsletter_signup = NewsletterSignup.new
-    @advertisement = Advertisement.displayed.first
+    @advertisements = Advertisement.displayed
   end
 
   def current_member
