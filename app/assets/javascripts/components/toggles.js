995a7913ecd4ef0bc41f7af4a054c2241b34c192
pauloptimised <pauloptimised@users.noreply.github.com>
2023-04-05 15:57:12 +0100

    Allow empty tables to prevent attendee details fieldset

--- app/assets/javascripts/components/toggles.js
+++ app/assets/javascripts/components/toggles.js
@@ -1,4 +1,4 @@
-$(document).on("click", ".toggle-class", function(e) {
+$(document).on("click", ".toggle-class", function (e) {
   /*
    * Example usage:
    * <a href="#div-to-go-to" data-container="#id" data-class="hidden" data-return="false">Toggle .hidden on #id and stop</a>
@@ -7,18 +7,18 @@ $(document).on("click", ".toggle-class", function(e) {
    * <a href="#div-to-go-to" data-container=".links" data-class="hidden" data-return="true">Toggle .hidden on .links and scroll to #div-to-go-to</a>
    */
 
-  var toggleContainer = $(this).attr('data-container');
-  var toggleClass     = $(this).attr('data-class');
-  var dataReturn      = $(this).attr('data-return');
+  var toggleContainer = $(this).attr("data-container");
+  var toggleClass = $(this).attr("data-class");
+  var dataReturn = $(this).attr("data-return");
 
   $(toggleContainer).toggleClass(toggleClass);
 
-  if (dataReturn === 'false') {
+  if (dataReturn === "false") {
     e.preventDefault();
   }
 });
 
-$(document).on("click", ".add-event-attendees", function(e) {
+$(document).on("click", ".add-event-attendees", function (e) {
   /*
    * Example usage:
    * <a href="#div-to-go-to" data-container="#id" data-class="hidden" data-return="false">Toggle .hidden on #id and stop</a>
@@ -27,20 +27,33 @@ $(document).on("click", ".add-event-attendees", function(e) {
    * <a href="#div-to-go-to" data-container=".links" data-class="hidden" data-return="true">Toggle .hidden on .links and scroll to #div-to-go-to</a>
    */
 
-  var toggleContainer = $(this).attr('data-container');
-  var toggleClass     = $(this).attr('data-class');
-  var dataReturn      = $(this).attr('data-return');
+  var toggleContainer = $(this).attr("data-container");
+  var toggleClass = $(this).attr("data-class");
+  var dataReturn = $(this).attr("data-return");
 
   $(toggleContainer).toggleClass(toggleClass);
 
-  if($(this).attr('data-clicked') === undefined) {
-    $('.add_fields').click();
+  if ($(this).attr("data-clicked") === undefined) {
+    $(".add_fields").click();
     setMainBookerDetails();
-    $('.booker-population, .remove_fields, .guest-attendee').hide();
-    $(this).attr('data-clicked', 'true');
+    $(".booker-population, .remove_fields, .guest-attendee").hide();
+    $(this).attr("data-clicked", "true");
   }
 
-  if (dataReturn === 'false') {
+  if (dataReturn === "false") {
     e.preventDefault();
   }
+});
+
+$(document).on("click", ".add-event-attendees-and-proceed", function (e) {
+  e.preventDefault();
+
+  if ($(this).attr("data-clicked") === undefined) {
+    $(".add_fields").click();
+    setMainBookerDetails();
+    $(".event-agenda-attendee-fields.nested-fields").hide();
+    $(this).attr("data-clicked", "true");
+  }
+
+  $("#new_event_booking").trigger("submit");
 });
