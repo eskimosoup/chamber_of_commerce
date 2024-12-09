2795d4fb27c1990e1832751d2d83b45948324b57
pauloptimised <pauloptimised@users.noreply.github.com>
2024-12-09 16:15:55 +0000

    Revert

--- app/controllers/event_groups_controller.rb
+++ app/controllers/event_groups_controller.rb
@@ -25,11 +25,11 @@ class EventGroupsController < ApplicationController
   end
 
   def events
-    if event_group.area == 'International'
-      ::Event.upcoming.where('events.id IN (?)', event_group_event_ids)
-    else
+    # if event_group.area == 'International'
+    #   ::Event.upcoming.where('events.id IN (?)', event_group_event_ids)
+    # else
       ::Event.upcoming.where('events.id IN (?) OR event_agendas_count = ?', event_group_event_ids, 0)
-    end
+    # end
   end
 
   def event_group_event_ids
